10ecada04703294fc713293f6d65cb11c54ddb8f
yuichi oyama <yuichi19991208@gmail.com>
2020-10-23 11:53:12 +0900

    modelにデータ処理をするメソッドを定義しテストでそのメソッドが使えるか確認

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ApplicationRecord
+  def self.count_titleA
+    blogs = Blog.where(title: 'タイトルA')
+    blogs.count
+  end
 end
--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Blog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Blog do
+  it "counts titleA" do
+    Blog.create(title: 'タイトルA', content: 'あいうえお')
+    Blog.create(title: 'タイトルAA', content: 'かきくけこ')
+    Blog.create(title: 'タイトル', content: 'さしすせそ')
+    Blog.create(title: 'タイトルB', content: 'たちつてと')
+    Blog.create(title: 'タイトルA', content: 'なにぬねの')
+    expect(Blog.count_titleA).to eq 2
+  end
+  it "counts titleA When titleA does not exist " do
+    Blog.create(title: 'タイトルC', content: 'あいうえお')
+    Blog.create(title: 'タイトルAA', content: 'かきくけこ')
+    Blog.create(title: 'タイトル', content: 'さしすせそ')
+    Blog.create(title: 'タイトルB', content: 'たちつてと')
+    Blog.create(title: 'タイトルC', content: 'なにぬねの')
+    expect(Blog.count_titleA).to eq 0
+  end
 end
